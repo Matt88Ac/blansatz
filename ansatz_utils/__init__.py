0fa9f62442e36d277239e54c77ba7a285d89d379
Matan Mizrahi <58215900+Matt88Ac@users.noreply.github.com>
2025-11-23 23:36:10 +0200

    Update __init__.py

--- ansatz_utils/__init__.py
+++ ansatz_utils/__init__.py
@@ -1,5 +1,6 @@
-from .nn_utils import get_activation
+from .nn_utils import get_activation, vandermonde_determinant
 from .neural_networks import MLP
 from .permutation_actions import random_negative_permutation, random_transposition, permutation_sign
 from .permutation_actions import vectorized_permutation_sign, all_transpositions
 from .projective_layers import AnInvariantEmbedding
+from .frames import NonLinearWeightedFrame
