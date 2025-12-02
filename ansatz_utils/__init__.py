9b7709008503626105aa665f6001827c74de8b8e
Matan Mizrahi <58215900+Matt88Ac@users.noreply.github.com>
2025-12-02 20:51:45 +0200

    Update __init__.py

--- ansatz_utils/__init__.py
+++ ansatz_utils/__init__.py
@@ -1,5 +1,5 @@
 from .nn_utils import get_activation, vandermonde_determinant, prod_all_but_one, uniform_sphere_point
-from .neural_networks import MLP
+from .neural_networks import MLP, DeepSet
 from .permutation_actions import random_negative_permutation, random_transposition, permutation_sign
 from .permutation_actions import vectorized_permutation_sign, all_transpositions
 from .projective_layers import AnInvariantEmbedding, ProjectiveSorting, alternation_separation
