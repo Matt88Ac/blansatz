ea17e09684b258daefe6913ea5a7709f6ed00823
Matan Mizrahi <58215900+Matt88Ac@users.noreply.github.com>
2025-12-02 05:55:58 +0200

    Update neural_networks.py

--- ansatz_utils/neural_networks.py
+++ ansatz_utils/neural_networks.py
@@ -5,6 +5,62 @@ from torch import nn
 from .nn_utils import get_activation
 
 
+class DeepSet(nn.Module):
+    """ DeepSet class.
+
+         Attributes:
+             in_dim (int):
+                 Size of each input sample.
+             out_dim (int):
+                 Size of each output sample.
+             layer_dims (list[int]):
+                 Each input/output dimension.
+     """
+
+    def __init__(self, in_dim: int, out_dim: int, hidden_layers: Optional[list[int]] = None,
+                 biases: Optional[Union[bool, Iterable[bool]]] = True,
+                 activation: Optional[str] = 'leakyrelu', activation_constant: Optional[float] = 0.01,
+                 device=torch.device('cpu'), dtype=torch.float64):
+        """
+        Args:
+            in_dim (int):
+                Size of each input sample.
+            out_dim (int):
+                Size of each output sample.
+            hidden_layers (list[int], Optional):
+                The width of each hidden layer. If None, then the MLP is equivalent to a linear layer. Default: None.
+            biases (bool, Iterable[bool], Optional):
+                When set to True, each linear layer learns an additive bias, and doesn't when set to False. Another
+                option is to specify for each hidden layer, by setting a boolean iterable. Default: True.
+            activation: (str, Optional):
+                Either one of 'leakyrelu', 'elu', 'relu', 'silu', 'sigmoid', 'softplus', 'mish', 'identity' or 'tanh'.
+                Default: 'leakyrelu'.
+            activation_constant: (float, Optional):
+                A tunable hyperparameter that some of the mentioned activation functions use. Neglected when irrelevant.
+                Default: 0.01.
+            device (str, torch.device, Optional): The device. Default: 'cpu'.
+            dtype (str, torch.dtype, Optional): The dtype. Default: torch.float64.
+
+        Examples:
+            >>> ds = DeepSet(5, 7, [3, 10, 2], True)
+            >>> ds
+            DeepSet(
+              (layers): Sequential(
+                (0): Linear(in_features=5, out_features=3, bias=True)
+                (1): LeakyReLU(negative_slope=0.01)
+                (2): Linear(in_features=3, out_features=10, bias=True)
+                (3): LeakyReLU(negative_slope=0.01)
+                (4): Linear(in_features=10, out_features=2, bias=True)
+                (5): LeakyReLU(negative_slope=0.01)
+                (6): Linear(in_features=2, out_features=7, bias=True)
+              )
+            )
+        """
+        super(DeepSet, self).__init__()
+        if hidden_layers is None:
+            hidden_layers = []
+
+
 class MLP(nn.Module):
     """ MLP class.
 
@@ -79,8 +135,7 @@ class MLP(nn.Module):
         self.layers = nn.Sequential(*layers).to(device=device, dtype=dtype)
 
     def forward(self, x):
-        out = self.layers(x)
-        return out
+        return self.layers(x)
 
 
 if __name__ == '__main__':
