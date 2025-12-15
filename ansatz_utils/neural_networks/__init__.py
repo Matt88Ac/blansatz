238a94c4086703edffad7ca31e3345c50e0ca4d6
Matan Mizrahi <58215900+Matt88Ac@users.noreply.github.com>
2025-12-15 17:02:27 +0200

    Update __init__.py

--- ansatz_utils/neural_networks/__init__.py
+++ ansatz_utils/neural_networks/__init__.py
@@ -1,4 +1,16 @@
-from .activations import get_activation
+from .activations import get_activation, nn
 from .aggregations import get_aggregation
 from .mlp import MLP
 from .deepsets import DeepSets
+
+
+def get_model(model_name: str, *args, **kwargs) -> nn.Module:
+    if model_name.lower() == 'mlp':
+        return MLP(*args, **kwargs)
+
+    elif model_name.lower() in ['ds', 'deepsets', 'deepset']:
+        return DeepSets(*args, **kwargs)
+
+
+
+
