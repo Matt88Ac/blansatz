ef2ad98b25d32e7cbc02c6c85c2e12a75ffec3f2
Matan Mizrahi <58215900+Matt88Ac@users.noreply.github.com>
2025-12-15 17:02:31 +0200

    Update bi_lipschitz_model.py

--- bi_lipschitz_model.py
+++ bi_lipschitz_model.py
@@ -3,12 +3,13 @@ from typing import Optional
 import torch
 from torch import nn
 
-from ansatz_utils import MLP, random_negative_permutation, AnInvariantEmbedding
+from ansatz_utils import get_model, random_negative_permutation, AnInvariantEmbedding
 
 
 class BiLipschitzAntiSymmetricModel(nn.Module):
     def __init__(self, in_dim: int, in_channels: int, out_dim: int, embedding_dim: Optional[int] = None,
-                 device: Optional = torch.device('cpu'), dtype: Optional = torch.float64, **mlp_kwargs):
+                 model_name: Optional[str] = 'mlp',
+                 device: Optional = torch.device('cpu'), dtype: Optional = torch.float64, **model_kwargs):
         """
         Args:
             in_dim:
@@ -17,28 +18,32 @@ class BiLipschitzAntiSymmetricModel(nn.Module):
             embedding_dim:
             device:
             dtype:
-            **mlp_kwargs:
+            **model_kwargs:
         """
         super(BiLipschitzAntiSymmetricModel, self).__init__()
         if embedding_dim is None:
             embedding_dim = (2 * in_dim * in_channels) + 1
 
-        mlp_kwargs['in_dim'] = embedding_dim
-        mlp_kwargs['out_dim'] = out_dim
-        mlp_kwargs['device'] = device
-        mlp_kwargs['dtype'] = dtype
+        model_kwargs['in_dim'] = embedding_dim
+        model_kwargs['out_dim'] = out_dim
+        model_kwargs['device'] = device
+        model_kwargs['dtype'] = dtype
+        if model_name in ['ds', 'deepsets', 'deepset']:
+            model_kwargs['new_dim'] = True
+            model_kwargs['in_dim'] = 1
         self.tau = random_negative_permutation(in_channels, device)
         self.psi = AnInvariantEmbedding(in_dim, in_channels, embedding_dim).to(device=device, dtype=dtype)
-        self.mlp = MLP(**mlp_kwargs)
+        self.model = get_model(model_name, **model_kwargs)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return 0.5 * (self.mlp(self.psi(x)) - self.mlp(self.psi(x[..., self.tau])))
+        return (self.model(self.psi(x)) - self.model(self.psi(x[..., self.tau]))) / 2
 
 
 if __name__ == '__main__':
     b, d, n = 10, 3, 15
-    model = BiLipschitzAntiSymmetricModel(d, n, 4, hidden_layers=[5, 100, 3], device='cpu')
+    model = BiLipschitzAntiSymmetricModel(d, n, 4, hidden_layers=[5, 100, 3], device='cpu', model_name='ds',
+                                          aggregation='avg')
 
     temp = torch.rand(b, d, n, dtype=torch.float64, device='cpu')*1000
 
-    print(model(temp).shape)
+    print(model(temp))
