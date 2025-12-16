f0d027c6bbef94303662c3c54919640007ab5e92
Matan Mizrahi <58215900+Matt88Ac@users.noreply.github.com>
2025-12-16 18:52:56 +0200

    Update __init__.py

--- experiments/__init__.py
+++ experiments/__init__.py
@@ -1,21 +1,14 @@
 import os
 
-import numpy as np
 from numpy import linalg as la
-from .norm_cross_product_discontinuity import *
-from .determinant import *
-from .cross_product import *
+
+from .data_generator import EXPERIMENTS, generate_datasets
 
 PATH = os.path.dirname(__file__)
-EXPERIMENTS = {
-    'determinant': la.det,
-    'cross_product': multi_cross,
-    'norm_cross_product_discontinuity': discontinuous_multi_cross
-}
 
 for f in EXPERIMENTS.keys():
     if not os.path.exists(PATH + os.sep + f + os.sep + 'data'):
         os.mkdir(PATH + os.sep + f + os.sep + 'data')
-        os.mkdir(PATH + os.sep + f + os.sep + 'data' + os.sep + 'train')
-        os.mkdir(PATH + os.sep + f + os.sep + 'data' + os.sep + 'validation')
-        os.mkdir(PATH + os.sep + f + os.sep + 'data' + os.sep + 'test')
+
+
+
