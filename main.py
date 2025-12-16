5fcbdd02d8eb33c1652fbc8550f7379a70dbe1ce
Matan Mizrahi <58215900+Matt88Ac@users.noreply.github.com>
2025-12-16 19:26:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from experiments import generate_datasets
 
 if __name__ == '__main__':
-    generate_datasets('determinant', 5, 5, 10, 5, 5)
+    pass
+    # generate_datasets('determinant', 2, 2, 100_000, 10_000, 10_000)
+    generate_datasets('determinant', 10, 10, 100_000, 10_000, 20_000,
+                      lower=-1.4, upper=1.4)
+    generate_datasets('determinant', 15, 15, 100_000, 10_000, 20_000,
+                      lower=-1.2, upper=1.2)
