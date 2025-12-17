cccb2d742c5c0de450e7d608f836d691bbad8ab4
Matan Mizrahi <58215900+Matt88Ac@users.noreply.github.com>
2025-12-17 10:11:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from experiments import generate_datasets
 if __name__ == '__main__':
     pass
     # generate_datasets('determinant', 2, 2, 100_000, 10_000, 10_000)
-    generate_datasets('determinant', 10, 10, 100_000, 10_000, 20_000,
-                      lower=-1.4, upper=1.4)
-    generate_datasets('determinant', 15, 15, 100_000, 10_000, 20_000,
-                      lower=-1.2, upper=1.2)
+    # generate_datasets('determinant', 10, 10, 100_000, 10_000, 20_000,
+    #                   lower=-1.4, upper=1.4)
+    # generate_datasets('determinant', 15, 15, 100_000, 10_000, 20_000,
+    #                   lower=-1.2, upper=1.2)
+
+    # generate_datasets('determinant', 30, 30, 100_000, 10_000, 20_000,
+    #                   lower=-0.3, upper=1.05)
+
+    # generate_datasets('norm_cross_product_discontinuity', 10, 3, 100_000, 10_000, 20_000, -1.5, 1.5)
+    # generate_datasets('norm_cross_product_discontinuity', 50, 3, 100_000, 10_000, 20_000, -1.3, 1.3)
+    generate_datasets('norm_cross_product_discontinuity', 300, 3, 100_000, 10_000, 20_000, -1.3, 1.5)
