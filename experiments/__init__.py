7d40ab787d38f0177c668a62446727f0e1ba78b4
Matan Mizrahi <58215900+Matt88Ac@users.noreply.github.com>
2025-12-21 01:03:33 +0200

    Update __init__.py

--- experiments/__init__.py
+++ experiments/__init__.py
@@ -1,5 +1,7 @@
 from .experiment_utils import (get_optimizer, get_loss, get_lr_scheduler, get_dtype,
-                               AVAILABLE_LOSSES, AVAILABLE_OPTIMIZERS,  AVAILABLE_LR_SCHED)
+                               AVAILABLE_LOSSES, AVAILABLE_OPTIMIZERS, AVAILABLE_LR_SCHED)
 
 from .general_lightning_model import (GeneralTrainer, LightningOnVandermondeModel,
                                       LightningBiLipschitzAntiSymmetricModel, LightningAfaNetModel)
+
+from .run_experiments import run_experiment
