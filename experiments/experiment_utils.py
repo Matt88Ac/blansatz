e25b61279c08433e3a3408709320062d257fcad4
Matan Mizrahi <58215900+Matt88Ac@users.noreply.github.com>
2025-12-20 10:50:21 +0200

    Create experiment_utils.py

---
+++ experiments/experiment_utils.py
@@ -0,0 +1,55 @@
+from functools import partial
+
+import torch
+from torch.optim import Adam, AdamW, Adamax, Adagrad, Rprop, RMSprop, SGD, NAdam
+from torch.optim.lr_scheduler import ReduceLROnPlateau, CosineAnnealingWarmRestarts, CosineAnnealingLR
+
+AVAILABLE_OPTIMIZERS = {'adam', 'adamax', 'adamw', 'adagrad', 'nadam', 'rmsprop', 'rprop', 'sgd'}
+AVAILABLE_LR_SCHED = {'reduce', 'cos_res', 'cos'}
+AVAILABLE_LOSSES = {'mse', 'l1', 'huber', 'smooth_l1', 'mare'}
+
+
+
+
+def get_loss(loss: str) -> torch.nn.Module:
+    assert loss.lower() in AVAILABLE_LOSSES, (
+        NotImplementedError("Choose on of mse, l1, huber and smooth_l1 as loss functions"))
+
+    if loss.lower() == 'mse':
+        return torch.nn.MSELoss()
+    elif loss.lower() == 'l1':
+        return torch.nn.L1Loss()
+    elif loss.lower() == 'huber':
+        return torch.nn.HuberLoss()
+    elif loss.lower() == 'smooth_l1':
+        return torch.nn.SmoothL1Loss(beta=0.5)
+    elif loss.lower() == 'mare':
+        return MeanAbsoluteRelativeError()
+
+
+def get_optimizer(optimizer: AVAILABLE_OPTIMIZERS, *args, **kwargs) -> partial:
+    if optimizer.lower() == 'adam':
+        return partial(Adam, *args, **kwargs)
+    elif optimizer.lower() == 'adamw':
+        return partial(AdamW, *args, **kwargs)
+    elif optimizer.lower() == 'adamax':
+        return partial(Adamax, *args, **kwargs)
+    elif optimizer.lower() == 'adagrad':
+        return partial(Adagrad, *args, **kwargs)
+    elif optimizer.lower() == 'nadam':
+        return partial(NAdam, *args, **kwargs)
+    elif optimizer.lower() == 'rmsprop':
+        return partial(RMSprop, *args, **kwargs)
+    elif optimizer.lower() == 'rprop':
+        return partial(Rprop, *args, **kwargs)
+    elif optimizer.lower() == 'sgd':
+        return partial(SGD, *args, **kwargs)
+
+
+def get_lr_scheduler(lr_scheduler: AVAILABLE_LR_SCHED, *args, **kwargs) -> partial:
+    if lr_scheduler.lower() == 'reduce':
+        return partial(ReduceLROnPlateau, *args, **kwargs)
+    elif lr_scheduler.lower() == 'cos_res':
+        return partial(CosineAnnealingWarmRestarts, *args, **kwargs)
+    elif lr_scheduler.lower() == 'cos':
+        return partial(CosineAnnealingLR, *args, **kwargs)
