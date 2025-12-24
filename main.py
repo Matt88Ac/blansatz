3c051472a0cfcdd7bcdbfccbf84f40ad89fa6de2
Matan Mizrachi <STAFF\mmizrachi@newton.csf.technion.ac.il>
2025-12-24 13:30:26 +0200

    running

--- main.py
+++ main.py
@@ -6,21 +6,22 @@ from experiments_data import generate_datasets
 if __name__ == '__main__':
     pass
     # generate_datasets('determinant', 2, 2, 100_000, 10_000, 10_000)
-    # generate_datasets('determinant', 10, 10, 100_000, 10_000, 20_000,
-    #                   lower=-1.4, upper=1.4)
-    # generate_datasets('determinant', 15, 15, 100_000, 10_000, 20_000,
-    #                   lower=-1.2, upper=1.2)
+    generate_datasets('determinant', 10, 10, 100_000, 10_000, 20_000,
+                       lower=-1.4, upper=1.4)
+    generate_datasets('determinant', 15, 15, 100_000, 10_000, 20_000,
+                       lower=-1.2, upper=1.2)
 
-    # generate_datasets('determinant', 30, 30, 100_000, 10_000, 20_000,
-    #                   lower=-0.3, upper=1.05)
+    generate_datasets('determinant', 30, 30, 100_000, 10_000, 20_000,
+                       lower=-0.3, upper=1.05)
 
-    # generate_datasets('norm_cross_product_discontinuity', 10, 3, 100_000, 10_000, 20_000, -1.5, 1.5)
-    # generate_datasets('norm_cross_product_discontinuity', 50, 3, 100_000, 10_000, 20_000, -1.3, 1.3)
-    # generate_datasets('norm_cross_product_discontinuity', 300, 3, 100_000, 10_000, 20_000, -1.3, 1.5)
+    generate_datasets('norm_cross_product_discontinuity', 10, 3, 100_000, 10_000, 20_000, -1.5, 1.5)
+    generate_datasets('norm_cross_product_discontinuity', 50, 3, 100_000, 10_000, 20_000, -1.3, 1.3)
+    generate_datasets('norm_cross_product_discontinuity', 300, 3, 100_000, 10_000, 20_000, -1.3, 1.5)
 
-    # generate_datasets('cross_product', 10, 3, 100_000, 10_000, 20_000, -1.5, 1.5)
-    # generate_datasets('cross_product', 50, 3, 100_000, 10_000, 20_000, -1.3, 1.3)
-    # generate_datasets('cross_product', 300, 3, 100_000, 10_000, 20_000, -1.3, 1.5)
+    generate_datasets('cross_product', 10, 3, 100_000, 10_000, 20_000, -1.5, 1.5)
+    generate_datasets('cross_product', 50, 3, 100_000, 10_000, 20_000, -1.3, 1.3)
+    generate_datasets('cross_product', 300, 3, 100_000, 10_000, 20_000, -1.3, 1.5)
+    exit(0)
 
     run_experiments.run_experiment(experiment='determinant', ansatz_name='bl',
                                    n_elements=10, dim=10, batch_size=256,
