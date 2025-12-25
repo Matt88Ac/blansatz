5ebe998471998ee1f1221216e580e3e1b04989ba
Matan Mizrahi <58215900+Matt88Ac@users.noreply.github.com>
2025-12-25 06:35:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,17 +23,20 @@ if __name__ == '__main__':
     # generate_datasets('cross_product', 300, 3, 100_000, 10_000, 20_000, -1.3, 1.5)
     # exit(0)
 
-    run_experiments.run_experiment(experiment='determinant', ansatz_name='afanet',
-                                   n_elements=2, dim=2, batch_size=128,
+    run_experiments.run_experiment(experiment='determinant',
+                                   ansatz_name='afanet',
+                                   n_elements=10, dim=10, batch_size=512,
                                    # embedding_dim=16,
-                                   optimizer_kwargs=dict(optimizer='adam', lr=1e-3, amsgrad=True),
-                                   lr_scheduler_kwargs=dict(lr_scheduler='reduce', factor=0.8, patience=1,
-                                                            min_lr=1e-5, ),
-                                   loss='l1', extra_metrics=['mse', 'l1', 'mare'],
-                                   # model_name='ds', aggregation='max',
+                                   optimizer_kwargs=dict(optimizer='adam', lr=1e-3),
+                                   lr_scheduler_kwargs=dict(lr_scheduler='reduce', factor=0.3, patience=0,
+                                                            min_lr=1e-6),
+                                   early_stopping_patience=15,
+                                   loss='mse', extra_metrics=['mse', 'l1', 'mare'],
                                    model_name='mlp',
                                    an_invariant=True, frame_name='nonlinear',
-                                   hidden_layers=[64, 64, 64], activation='tanh', biases='all_but_last',
-                                   device='cuda', dtype=torch.float32,
-                                   # persistent_workers=False, n_workers=8
+                                   hidden_layers=[256, 256], activation='tanh', biases='all_but_last',
+                                   # single_model=False, trainable_weights=True,
+                                   max_epochs=100,
+                                   device='cpu', dtype=torch.float64,
+                                   persistent_workers=True, n_workers=16, pin_memory=True
                                    )
