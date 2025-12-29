c8ac3a1d0d5939efe572d922b84ee87e3b853a3d
Matan Mizrahi <58215900+Matt88Ac@users.noreply.github.com>
2025-12-29 22:30:09 +0200

    Update permutation_actions.py

--- ansatz_utils/permutation_actions.py
+++ ansatz_utils/permutation_actions.py
@@ -21,17 +21,18 @@ def permutation_sign(p: torch.Tensor) -> torch.Tensor:
         >>> sign_p = permutation_sign(permutation)
     """
     dim = len(p)
-    count = torch.zeros(1, device=p.device, dtype=torch.int)
-    visited = torch.zeros(dim, device=p.device, dtype=torch.bool)
-    true_tensor = torch.ones(1, device=p.device, dtype=torch.int)
+    count = 0
+    visited = torch.zeros(dim, dtype=torch.bool)
+    true_tensor = torch.ones(1)[0]
     for i in range(dim):
         if not visited[i]:
             count += 1
             k = i
             while not visited[k]:
-                visited = visited.select_scatter(true_tensor.clone()[0], 0, k)
-                k = p[k].clone()
-    sign = - 2 * ((dim - count) % 2) + 1
+                visited = visited.select_scatter(true_tensor, 0, k)
+                k = p[k]
+
+    sign = torch.tensor(- 2 * ((dim - count) % 2) + 1, device=p.device, dtype=torch.int8)
     return sign
 
 
@@ -91,32 +92,29 @@ def random_transposition(dim: int, device: Optional[Union[str, torch.device]] = 
 
 
 @torch.jit.script
-def vmap_permutation_sign(p: torch.Tensor) -> torch.Tensor:
-    """ The function computes the signs of a given tensor of permutations.
-
-    Args:
-        p (torch.Tensor):
-            A k-D tensor of arbitrary last-dimension n, that contains distinct Long/Int elements from {0, ..., n-1}.
-
-    Returns:
-            -1 if the number of inversions in p is odd, or 1 otherwise.
-
-    Example:
-        n = 10;
-        permutation = torch.randperm(n);
-        sign_p = permutation_sign(permutation);
-    """
-    signs = torch.stack([
-        permutation_sign(p_i) for p_i in torch.unbind(p, dim=0)
-    ], dim=0)
-    return signs
-
-
 def vectorized_permutation_sign(p: torch.Tensor) -> torch.Tensor:
-    shape = p.shape
-    if len(shape) == 1:
+    """
+    The function computes the signs of a given tensor of permutations.
+
+        Args:
+            p (torch.Tensor):
+                A k-D tensor of arbitrary last-dimension n, that contains distinct Long/Int elements from {0, ..., n-1}.
+
+        Returns:
+                -1 if the number of inversions in p is odd, or 1 otherwise.
+
+        Example:
+            >>> n = 10
+            >>> permutation = torch.randperm(n)
+            >>> sign_p = permutation_sign(permutation)
+        """
+
+    if p.dim() == 1:
         return permutation_sign(p)
-    signs = vmap_permutation_sign(p.reshape(-1, shape[-1])).reshape(shape[:-1])
+
+    signs = torch.stack([
+        permutation_sign(p_i) for p_i in torch.unbind(p.contiguous().view(-1, p.size(-1)), dim=0)
+    ], dim=0).view_as(p[..., 0])
     return signs
 
 
@@ -124,11 +122,12 @@ if __name__ == '__main__':
     N_tests = 1000
     D = 100
     perms = torch.vstack([torch.randperm(D) for j in range(N_tests)])
+    # perms = perms.unsqueeze(1).repeat([1, 2, 1])
     S = vectorized_permutation_sign(perms)
-
     ID_MAT = torch.eye(D)
     for j in range(N_tests):
         s1 = ID_MAT[perms[j]].det()
         s2 = S[j]
-        if s2 != s1:
-            print(s1, s2)
+        # print(s1, s2)
+        assert s1 == s2
+
