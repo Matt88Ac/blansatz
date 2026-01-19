5e1fe5348f1aef414371989a78f16d716efc3535
Matan Mizrahi <58215900+Matt88Ac@users.noreply.github.com>
2026-01-19 12:28:13 +0200

    Update experiment_utils.py

--- experiments/experiment_utils.py
+++ experiments/experiment_utils.py
@@ -60,7 +60,7 @@ class MeanSquaredLogLoss(torch.nn.Module):
 
     def __init__(self):
         super(MeanSquaredLogLoss, self).__init__()
-        self.epsilon = 1e-10
+        self.epsilon = 1e-12
         self.mse = torch.nn.MSELoss()
 
     def forward(self, prediction: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
@@ -77,7 +77,7 @@ class SignedMeanSquaredLogLoss(torch.nn.Module):
         self.mse = torch.nn.MSELoss()
 
     def forward(self, prediction: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-        return self.msl(prediction, target) + self.mu * self.mse(prediction.sign(), target.sign())
+        return (1-self.mu)*self.msl(prediction, target) + self.mu * self.mse(prediction, target)
 
 
 def get_loss(loss: str) -> torch.nn.Module:
