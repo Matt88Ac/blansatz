9e64d458c372699d04e07672dbc44546e6cf45f5
Matan Mizrachi <matt8ac@gmail.com>
2026-06-23 19:24:09 +0300

    Update __init__.py

--- ansatz_utils/__init__.py
+++ ansatz_utils/__init__.py
@@ -1,7 +1,7 @@
-from .nn_utils import vandermonde_determinant, linear_wsop_sub_weights, uniform_sphere_sampling, AllDifferences
+from .nn_utils import vandermonde_determinant, uniform_sphere_sampling, AllDifferences
 from .neural_networks import *
 from .permutation_actions import (random_negative_permutation, random_transposition, permutation_sign,
                                   random_positive_permutation)
-from .permutation_actions import vectorized_permutation_sign, all_transpositions
+from .permutation_actions import vectorized_permutation_sign, all_transpositions, permute_ij
 from .projective_layers import AnInvariantEmbedding, ProjectiveSorting, alternation_separation
 from .frames import NonLinearWeightedFrame, LinearWeightedFrame, SoftNonLinearWeightedFrame
