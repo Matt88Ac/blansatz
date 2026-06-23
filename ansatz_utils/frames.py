c047768a42e55442be739f2ddf0161bf9a646734
Matan Mizrachi <matt8ac@gmail.com>
2026-06-23 19:24:06 +0300

    Update frames.py

--- ansatz_utils/frames.py
+++ ansatz_utils/frames.py
@@ -6,8 +6,7 @@ import torch
 from torch import Tensor
 from torch import nn
 
-from ansatz_utils import all_transpositions, ProjectiveSorting, linear_wsop_sub_weights, \
-    random_transposition
+from ansatz_utils import all_transpositions, ProjectiveSorting, random_transposition, permute_ij
 
 
 class AsWeightedFrame(nn.Module):
@@ -61,7 +60,7 @@ class AsWeightedFrame(nn.Module):
         # out_x = torch.take_along_dim(x.unsqueeze(1), permutations.unsqueeze(-2), -1)
 
         if not an_invariant:
-            framed_func = ((weights * signs) * ws_function(out_x)).sum(dim=1)
+            framed_func = ((weights * signs) * ws_function(out_x, x)).sum(dim=1)
         else:
             neg_perm = random_transposition(self.in_channels, device=self.weights.device)
             framed_func = torch.where(signs == 1,
@@ -73,16 +72,20 @@ class AsWeightedFrame(nn.Module):
 
         weights = weights.sum(dim=1)
         framed_func = torch.where(
-            torch.isclose(weights, torch.zeros_like(weights, requires_grad=False)),
-            framed_func * 0,
+            framed_func == 0.0, #torch.isclose(framed_func, torch.zeros_like(framed_func, requires_grad=False), atol=1e-20),
+            framed_func * 0.0,
             framed_func / weights,
-        ).nan_to_num(0, 0, 0)
+        )
 
         return framed_func
 
     @property
     def weights(self):
         return self.projection_sorting.weights
+
+    @property
+    def in_dim(self):
+        return self.projection_sorting.in_dim
 
     @property
     def get_device(self):
@@ -138,6 +141,7 @@ class WeakStabilizeWeightedFrame(nn.Module):
         super(WeakStabilizeWeightedFrame, self).__init__()
         self.weighted_frame = AsWeightedFrame(in_dim, in_channels, n_frames).to(device=device, dtype=dtype)
         self.transpositions = nn.Parameter(all_transpositions(in_channels, device=device), requires_grad=False)
+        self.identity = nn.Parameter(torch.arange(in_channels, device=device, dtype=torch.int64).unsqueeze(0), requires_grad=False)
         self.unstable_function = unstable_function.to(device, dtype)
         self.an_invariant = an_invariant
 
@@ -167,6 +171,22 @@ class WeakStabilizeWeightedFrame(nn.Module):
             x,
             self.an_invariant
         )
+
+    @property
+    def in_channels(self):
+        return self.weighted_frame.in_channels
+
+    @property
+    def in_dim(self):
+        return self.weighted_frame.in_dim
+
+    @property
+    def get_device(self):
+        return self.weighted_frame.get_device
+    @property
+    def get_dtype(self):
+        return self.weighted_frame.get_dtype
+
 
 
 class NonLinearWeightedFrame(WeakStabilizeWeightedFrame):
@@ -247,6 +267,8 @@ class LinearWeightedFrame(WeakStabilizeWeightedFrame):
         super(LinearWeightedFrame, self).__init__(unstable_function, in_dim, in_channels, n_frames, an_invariant,
                                                   device, dtype, *args, **kwargs)
 
+        self.delta = nn.Parameter(torch.rand(1, device=device, dtype=dtype))
+
     def stable_forward(self, sorted_x: Tensor, x: Tensor = None) -> Tensor:
         """
         This method implements the linear weakly-stabilizing transformation.
@@ -254,28 +276,59 @@ class LinearWeightedFrame(WeakStabilizeWeightedFrame):
             sorted_x (Tensor):
                 The sorted input tensor.
             x (Tensor): 
-                The original input tensor (used if the function is An-invariant).
+                The original input tensor (used for forward-pass if the function is An-invariant).
         Returns:
             Tensor: The weakly-stabilized output.
         """
-
-        weight_hat = torch.norm(sorted_x.diff(dim=-1), dim=-2, p='fro')
-        sorted_x = sorted_x.unsqueeze(-3)
-
-        if not self.an_invariant:
-            weight_hat = linear_wsop_sub_weights(weight_hat).unsqueeze(-1)
-            f_x: Tensor = self.unstable_function(sorted_x)
-            f_tx: Tensor = self.unstable_function(
-                torch.take_along_dim(sorted_x, self.transpositions.unsqueeze(0).unsqueeze(0).unsqueeze(-2), -1)
-            ).clone()
-
-            stable_func = f_x.squeeze(-2) - (weight_hat * (f_x + f_tx)).sum(dim=-2)
-
-        else:
+        if self.an_invariant:
             f_x: Tensor = self.unstable_function(x).unsqueeze(1)
-            f_tx: Tensor = self.unstable_function(x[..., self.transpositions[0]]).clone().unsqueeze(1)
-            stable_func = f_x - (weight_hat.sum(dim=-1, keepdim=True) * (f_x + f_tx))
+            f_tx: Tensor = self.unstable_function(permute_ij(x, 0, 1)).clone().unsqueeze(1)
+            # stable_func = f_x - (pdist.sum(dim=-1, keepdim=True) * (f_x + f_tx))
+            stable_func = f_x - (0.5 * (f_x + f_tx))
+            return stable_func
 
+
+        EPS = 1e-50
+
+        pdist = torch.cdist(x.transpose(-1, -2), x.transpose(-1, -2), p=2)
+        eye = torch.eye(self.in_channels, requires_grad=False, dtype=self.get_dtype, device=self.get_device)
+        eye.fill_diagonal_(float('inf'))
+        pdist += eye.unsqueeze(0).to(self.get_device, self.get_dtype)
+
+        eta_x = pdist.min(dim=-1, keepdim=False)[0].min(dim=-1, keepdim=True)[0].unsqueeze(-1)
+
+        pdist = torch.minimum(pdist,
+                              eta_x + nn.functional.softplus(self.delta)) / (eta_x + nn.functional.softplus(self.delta))
+        pdist = torch.tan(pdist * torch.pi / 2)
+
+        pdist = torch.triu(torch.true_divide(1, torch.where(pdist <= EPS, EPS, pdist)), diagonal=1)
+        pdist = 0.5 * pdist / torch.sum(pdist, dim=(-2,-1), keepdim=True)
+        pdist = nn.functional.relu(pdist - 2*EPS)
+
+        f_x: Tensor = self.unstable_function(sorted_x)
+
+        B, I, J = torch.nonzero(pdist, as_tuple=True)
+
+        K = 0
+        i0 = I[B == K]
+        j0 = J[B == K]
+        w = pdist[K, i0, j0].unsqueeze(-1).unsqueeze(-1)
+        tr0 = torch.stack([permute_ij(self.identity.clone(), _i, _j) for (_i, _j) in zip(i0, j0)],
+                          dim=0).unsqueeze(1)
+
+        f_tx = (w * self.unstable_function(torch.take_along_dim(sorted_x[K:K+1], tr0, -1))).sum(dim=0, keepdim=True)
+
+        for K in range(1, x.size(0)):
+            i0 = I[B == K]
+            j0 = J[B == K]
+            w = pdist[K, i0, j0].unsqueeze(-1).unsqueeze(-1)
+            tr0 = torch.stack([permute_ij(self.identity.clone(), _i, _j) for (_i, _j) in zip(i0, j0)],
+                              dim=0).unsqueeze(1)
+            f_tx = torch.cat([f_tx,
+                              (w * self.unstable_function(torch.take_along_dim(sorted_x[K:K+1], tr0, -1))).sum(dim=0, keepdim=True)],
+                             dim=0)
+
+        stable_func = 0.5 * f_x - f_tx
         return stable_func
 
 
@@ -354,13 +407,64 @@ if __name__ == '__main__':
     from neural_networks import MLP
     from projective_layers import AnInvariantEmbedding
 
-    b, d, n = 10, 3, 13
+    b, d, n = 10, 3, 5
+    print(f"b = {b}, d = {d}, n = {n}")
     # F = nn.Sequential(nn.Flatten(-2, -1), MLP(n * d, 1, [20, 20, 20]))
-    F = nn.Sequential(AnInvariantEmbedding(d, n, 50), MLP(50, 1, [20, 20, 20]))
-    X = torch.rand(b, d, n, dtype=torch.float64)
+    # # F = nn.Sequential(AnInvariantEmbedding(d, n, 50), MLP(50, 1, [20, 20, 20]))
+    # SF = LinearWeightedFrame(F, d, n, 130, an_invariant=False)
 
-    X[0, :, 0] = X[0, :, 3]
+    # X = torch.rand(b, d, n, dtype=torch.float64)
+    # X[0, :, 0] = X[0, :, 3]
+    # print(SF(X))
+    # print(SF(X).shape)
 
-    SF = LinearWeightedFrame(F, d, n, 130, an_invariant=True)
+    from time import time
+    from matplotlib import pyplot as plt
+    import numpy as np
+    from tqdm import tqdm
 
-    print(SF(X))
+
+    T = 10
+    bs = np.array([1, 5, 10, 20, 30, 40, 50, 75, 100, 128, 200, 500])
+    times = np.zeros(len(bs))
+    counter = tqdm(bs, colour='green', desc='Time')
+
+    F = nn.Sequential(nn.Flatten(-2, -1), MLP(n * d, 1, [20, 20, 20]))
+    # F = nn.Sequential(AnInvariantEmbedding(d, n, 50), MLP(50, 1, [20, 20, 20]))
+    SF = LinearWeightedFrame(F, d, n, 130, an_invariant=False)
+
+    for i, b in enumerate(counter):
+        all_t = 0.0
+        for _ in range(T):
+            x = torch.rand(b, d, n, dtype=torch.float64)
+            start = time()
+            SF(x)
+            all_t += time() - start
+        all_t /= T
+        times[i] = all_t
+
+    # plt.plot(bs, times)
+    plt.plot(bs, np.gradient(times, bs))
+    plt.grid()
+
+    # times = np.zeros(len(bs))
+    # counter = tqdm(enumerate(bs), colour='green', desc='Time')
+    # F = nn.Sequential(nn.Flatten(-2, -1), MLP(n * d, 1, [20, 20, 20]))
+    # F = nn.Sequential(AnInvariantEmbedding(d, n, 50), MLP(50, 1, [20, 20, 20]))
+    # SF = LinearWeightedFrame(F, d, n, 130, an_invariant=True)
+#
+    # for i, b in counter:
+    #     all_t = 0.0
+    #     for _ in range(T):
+    #         x = torch.rand(b, d, n, dtype=torch.float64)
+    #         start = time()
+    #         SF(x)
+    #         all_t += time() - start
+    #     all_t /= T
+    #     times[i] = all_t
+#
+    # plt.plot(bs, times)
+
+    plt.show()
+
+
